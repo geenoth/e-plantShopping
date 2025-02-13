bbc637e90a13e81977ff10863161db08d29cf94b
Geenoth <gvwgeenoth365@gmail.com>
2025-02-13 17:04:53 +0530

    Landing Page

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
-import preact from '@preact/preset-vite'
+import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [preact()],
+  base: "/shoppingreact",
+  plugins: [react()],
 })
